91c9a836446b4115bafa58585fe3a8606c88ef67
preciousyaks17 <johndoe@example.com>
2024-08-12 10:51:25 +0100

    updated the footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,7 +6,7 @@ import instagramLogo from "../../../assets/icons/logo-linkedin.svg";
 
 export default function Footer() {
   return (
-    <footer className="p-10 m-30 bg-green-600">
+    <footer className="p-10 m-30 bg-gradient-to-r from-green-600 to-green-950">
       <div className="flex flex-col md:flex-row items-center justify-center gap-6">
         <div className="flex items-center justify-center gap-4">
           <Image
