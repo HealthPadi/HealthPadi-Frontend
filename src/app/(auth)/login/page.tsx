52c2c3dbb03796f43d16cf1da5da2ad1c9cf4a6a
preciousyaks17 <johndoe@example.com>
2024-08-13 16:19:19 +0100

    changed an icon

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -57,14 +57,6 @@ export default function Login() {
                     height={20}
                   />
                 </Link>
-                {/* <Link href="/google" className="ml-2">
-                  <Image
-                    src={googleLogo}
-                    alt="Google logo"
-                    width={20}
-                    height={20}
-                  />
-                </Link> */}
               </p>
             </div>
           </div>
